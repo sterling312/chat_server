83044cfdbff0371e4530697397a61cd928265dcd
Gang Huang <doc.n.try@gmail.com>
2014-10-05 17:37:25 -0500

    init

---
+++ chat_server.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+import pdb
+import argparse
+import socket
+from flask import Flask, render_template, request, url_for, redirect, session, jsonify
+from tornado.ioloop import IOLoop
+from tornado.web import Application, RequestHandler, FallbackHandler
+from tornado.wsgi import WSGIContainer
+from tornado.websocket import WebSocketHandler
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-p','--port',help='server port',default=8080)
+parser.add_argument('--host',help='host ip',default=socket.gethostbyname(socket.gethostname()))
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template('chat.html')
+
+@app.route('/url')
+def url():
+    username = request.args.get('username','anonymous')
+    url = 'ws://%(host)s:%(port)d/ws' % dict(host=args.host,port=int(args.port))
+    return jsonify(url=url,username=username)
+
+class WS(WebSocketHandler):
+    cl = []
+    def open(self):
+        self.username = self.get_arguments('username')[0]
+        if not self in self.cl:
+            if self.authenticate():
+                self.cl.append(self)
+                self.write_message('connected')
+
+    def on_message(self,msg):
+        if not self.authenticated:
+            return 
+        for cl in self.cl:
+            cl.write_message(self.username+': '+msg)
+        
+    def on_close(self):
+        if self in self.cl:
+            self.cl.remove(self)
+
+    def authenticate(self):
+        self.authenticated = True
+        return self.authenticated
+
+if __name__=="__main__":
+    args = parser.parse_args()
+    server = Application([(r'/ws',WS),
+                          (r'/.*',FallbackHandler,dict(fallback=WSGIContainer(app)))])
+    server.listen(args.port,address=args.host)
+    print(args.host,args.port)
+    IOLoop.instance().start()
---
+++ static/chat.js
@@ -0,0 +1,42 @@
+var onopen = function (){
+    $('div#window div').remove();
+}
+
+var onmessage = function (evt){
+    var $div = $('<div></div>').text(evt.data);
+    $('div#window').append($div);
+}
+
+var ws;
+function open_ws(data){
+    var params = $.param({username:data.username});
+    ws = new WebSocket(data.url+'?'+params);
+    ws.onopen = onopen;
+    ws.onmessage = onmessage;
+}
+
+function login(username){
+    var params = $.param({username:username});
+    $.ajax('/url?'+params,{
+        method:'get',
+        dataType:'json',
+    }).done(open_ws);
+}
+
+$(document).ready(function(){
+    $('div.chat').hide();
+    $('button#send').click(function(){
+        var $text = $('input#message');
+        ws.send($text.val());
+        $text.text('');
+    });
+
+    $('button#login').click(function(){
+        var $user = $('input#username');
+        if ($user.val()){
+            login($user.val());
+        };
+        $('div.login').hide();
+        $('div.chat').show();
+    });
+});
